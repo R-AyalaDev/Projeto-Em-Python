33cd50ba118c1996b2c9cdff19bb1399d80904a2
richarlysonayala <richarlisonayala@gmail.com>
2023-12-10 11:11:58 -0300

    Corrigindo erro do cpf

--- arquivos/interface/__init__.py
+++ arquivos/interface/__init__.py
@@ -59,6 +59,7 @@ def leiaCPF(msg):
         try:
             if (len(c) != 14) and (c[3] != '.') and (c[7] != '.') and (c[11] != '-'):
                 print('\033[1;31mERRO! Digite um CPF válido!\033[1;30m')
+                
             
         except:
             print('\033[1;31mERRO! Digite um CPF válido!\033[1;30m')
